9d713b4d7ccb93b2fb8e80a92cea748d05ee0477
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2022-11-04 21:24:06 +0100

    feat: add docker compose for order service

--- start.sh
+++ start.sh
@@ -4,3 +4,4 @@ docker network create school
 docker compose -f shared.docker-compose.yaml up -d --build
 docker compose -f students.docker-compose.yaml up -d --build
 docker compose -f library.docker-compose.yaml up -d --build
+docker compose -f orders.docker-compose.yaml up -d --build
